209f494e5864b0f3764e13eaf91a5d89b14f7dbd
guna <shouryaguna@gmail.com>
2017-08-10 23:52:09 +0000

    added basic installation cookbook

---
+++ cookbooks/apache/metadata.rb
@@ -0,0 +1,7 @@
+name 'apache'
+maintainer 'guna'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
